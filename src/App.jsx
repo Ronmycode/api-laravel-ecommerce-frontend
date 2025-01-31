867ba9e48a53d98fea893e45be93d3649aada470
Ronmycode <67519456+Ronmycode@users.noreply.github.com>
2025-01-31 16:54:37 -0600

    CRUD products - added Product list component

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import "./App.css";
 import NavBar from "./components/Nav-Bar";
 import Footer from "./components/Footer";
 import "bootstrap/dist/css/bootstrap.min.css";
+import ProductList from "./components/ProductList";
 
 function App() {
   return (
     <>
       <NavBar />
+      <ProductList />
       <Footer />
     </>
   );
